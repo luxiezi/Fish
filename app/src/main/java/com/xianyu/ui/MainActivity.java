c803ec2122fdb6f4ecd5b73218a61c1cc8e7495d
luxiezi <529904239@qq.com>
2016-08-04 13:54:22 +0800

    1

--- app/src/main/java/com/xianyu/ui/MainActivity.java
+++ app/src/main/java/com/xianyu/ui/MainActivity.java
@@ -3,6 +3,7 @@ package com.xianyu.ui;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
@@ -14,71 +15,78 @@ import com.xianyu.ui.fragment.Main_fragment;
 import com.xianyu.ui.fragment.Me_fragment;
 import com.xianyu.ui.fragment.Search_fragment;
 
-public class MainActivity extends FragmentActivity implements OnCheckedChangeListener {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    private String TAG="MainActivity";
+public class MainActivity extends FragmentActivity implements OnCheckedChangeListener {
+    private String TAG = "MainActivity";
+    @BindView(R.id.radiogroup_fragment)
+    RadioGroup radiogroup_Fragment;
+    @BindView(R.id.main_rb_fragment)
+    RadioButton main_rb_ragment;
+    @BindView(R.id.search_rb_fragment)
+    RadioButton search_rb_fragment;
+    @BindView(R.id.dolla_rb_fragment)
+    RadioButton dolla_rb_fragment;
+    @BindView(R.id.car_rb_fragment)
+    RadioButton car_rb_fragment;
+    @BindView(R.id.me_rb_fragment)
+    RadioButton me_rb_fragment;
+    @BindView(R.id.framelayout)
+    FrameLayout framelayout;
+
     Main_fragment mainTabFragment;
     Search_fragment searchTabFragment;
     Me_fragment meTabFragment;
     Dolla_fragment dollaTabFragment;
     Car_fragment carTabFragment;
-    RadioGroup radioGroup;
-    RadioButton main_rb;
-    RadioButton search_rb;
-    RadioButton dolla_rb;
-    RadioButton car_rb;
-    RadioButton me_rb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.fish.xianyu.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
         initView();
 
     }
-    private void initView() {
-        radioGroup= (RadioGroup) findViewById(R.id.radiogroup_fragment);
-        main_rb= (RadioButton) findViewById(R.id.main_rb_fragment);
-        search_rb= (RadioButton) findViewById(R.id.search_rb_fragment);
-        dolla_rb= (RadioButton) findViewById(R.id.dolla_rb_fragment);
-        car_rb= (RadioButton) findViewById(R.id.car_rb_fragment);
-        me_rb= (RadioButton) findViewById(R.id.me_rb_fragment);
 
+    private void initView() {
 //        ((RadioButton) radioGroup.getChildAt(0)).setChecked(true);
         showFragment(R.id.main_rb_fragment);
-        radioGroup.setOnCheckedChangeListener(this);
+        radiogroup_Fragment.setOnCheckedChangeListener(this);
     }
 
     private void showFragment(int index) {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        switch (index){
+        switch (index) {
             case R.id.main_rb_fragment:
-                if (mainTabFragment==null) {
+                if (mainTabFragment == null) {
                     mainTabFragment = new Main_fragment();
                     ft.add(R.id.framelayout, mainTabFragment);
-                }else{
+                } else {
                     ft.replace(R.id.framelayout, mainTabFragment);
                 }
                 break;
             case R.id.search_rb_fragment:
-                if (searchTabFragment==null)
-                    searchTabFragment=new Search_fragment();
-                ft.replace(R.id.framelayout,searchTabFragment);
+                if (searchTabFragment == null)
+                    searchTabFragment = new Search_fragment();
+                ft.replace(R.id.framelayout, searchTabFragment);
                 break;
             case R.id.dolla_rb_fragment:
-                if (dollaTabFragment==null)
-                    dollaTabFragment=new Dolla_fragment();
-                ft.replace(R.id.framelayout,dollaTabFragment);
+                if (dollaTabFragment == null)
+                    dollaTabFragment = new Dolla_fragment();
+                ft.replace(R.id.framelayout, dollaTabFragment);
                 break;
             case R.id.car_rb_fragment:
-                if (carTabFragment==null)
-                    carTabFragment=new Car_fragment();
-                ft.replace(R.id.framelayout,carTabFragment);
+                if (carTabFragment == null)
+                    carTabFragment = new Car_fragment();
+                ft.replace(R.id.framelayout, carTabFragment);
                 break;
             case R.id.me_rb_fragment:
-                if (meTabFragment==null)
-                    meTabFragment=new Me_fragment();
-                ft.replace(R.id.framelayout,meTabFragment);
+                if (meTabFragment == null)
+                    meTabFragment = new Me_fragment();
+                ft.replace(R.id.framelayout, meTabFragment);
                 break;
         }
         ft.commit();
