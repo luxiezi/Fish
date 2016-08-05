8d3846736891f0e901a7b5d9fe8e82806470953d
qike <649381130@qq.com>
2016-08-05 16:55:50 +0800

    1

--- example/src/main/java/com/example/fo/ui/launch/DemoActivity.java
+++ example/src/main/java/com/example/fo/ui/launch/DemoActivity.java
@@ -1,5 +1,6 @@
 package com.example.fo.ui.launch;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.ListView;
@@ -37,17 +38,18 @@ public class DemoActivity extends BaseActivity
     @Override
     protected void onCreate(@Nullable Bundle bundle)
     {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(bundle);
 
         //模拟数据
         List<RateInfo> mRateInfos = new ArrayList<>();
-        mRateInfos.add(new RateInfo("货币名称","现汇买入价","现汇卖出价","现钞卖出价","30.22","20.98","2016-8-4 14:40:10"));
+        mRateInfos.add(new RateInfo("货币名称", "现汇买入价", "现钞买入价", "现汇卖出价", "现钞卖出价", "折算价", "发布时间"));
 
-        mRateInfos.add(new RateInfo("美元","50.21","10.01","20.22","30.22","20.98","2016-8-4 14:40:10"));
-        mRateInfos.add(new RateInfo("外星币","50.21","10.01","20.22","","20.98","2016-8-4 14:40:10"));
-        mRateInfos.add(new RateInfo("测试数据","","10.01","20.22","30.22","20.98","2016-8-4 14:40:10"));
-        mRateInfos.add(new RateInfo("测试超长超长文字","50.21","10.01","20.22","30.22","20.98","2016-8-4 14:40:10"));
-        mRateInfos.add(new RateInfo("短","50.21","10.01","20.22","","20.98","2016-8-4 14:40:10"));
+        mRateInfos.add(new RateInfo("美元", "50.21", "10.01", "20.22", "30.22", "20.98", "2016-8-4 14:40:10"));
+        mRateInfos.add(new RateInfo("外星币", "50.21", "10.01", "20.22", "", "20.98", "2016-8-4 14:40:10"));
+        mRateInfos.add(new RateInfo("测试数据", "", "10.01", "20.22", "30.22", "20.98", "2016-8-4 14:40:10"));
+        mRateInfos.add(new RateInfo("测试超长超长文字", "50.21", "10.01", "20.22", "30.22", "20.98", "2016-8-4 14:40:10"));
+        mRateInfos.add(new RateInfo("短", "50.21", "10.01", "20.22", "", "20.98", "2016-8-4 14:40:10"));
 
         DemoAdapter mAdapter = new DemoAdapter(mRateInfos);
 
