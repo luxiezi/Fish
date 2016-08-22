0a7a7aff46ad498c7551eed6a1967874e2219356
luxiezi <529904239@qq.com>
2016-08-22 10:11:47 +0800

    main_fragment

---
+++ app/src/main/java/com/xianyu/ui/main_fragment/Main_life_fragment.java
@@ -0,0 +1,19 @@
+package com.xianyu.ui.main_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lu on 2016/8/21.
+ */
+public class Main_life_fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(com.xianyu.resource.R.layout.main_life_fragment_layout,container,false);
+    }
+}
